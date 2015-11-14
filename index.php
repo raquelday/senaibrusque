2d986e7f89afc17102fcaefcd7e38dade4c3e5d2
Aluno <Aluno@10.1.20.34>
2015-11-13 22:42:06 -0200

    Atualizando index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Site Institucional do SENAI - Brusque</title>
+        
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/estilo.css"/>
+        <!--java script -->
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/meuscript.js"></script>
+        
+    </head>
+    <body>
+        <div class="container-fluid">
+            <header class="jumbotron text-center" id="topo">
+                <h1>SENAI/SC Brusque</h1>
+                <p>Alo Ha mundo do Bootstrap</p>
+                <form role="form">
+                    <div class="form-inline">
+                        
+                        <div class="input-group input-group-sm">
+                            <span class="input-group-addon"> 
+                                <span class="glyphicon glyphicon-envelope"></span>
+                            </span>
+                                
+                            <input type="e-mail"
+                                   class="form-control"
+                                   id="e-mail"
+                                   placeholder="seunome@seuemail.com"
+                                   >
+                            
+                        </div>
+                    </div>
+                        
+                           <button type="submit" class="btn btn-success">Cadastrar</button>
+                </form>
+            </header>
+        </div>
+    </body>
+</html>
