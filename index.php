334f89638dbe1a7fd580f00f47147722ee3cf024
raquel day <raquel.day@hotmail.com>
2015-11-13 22:42:06 -0200

    closes #2

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Site Institucional do SENAI - Brusque</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
