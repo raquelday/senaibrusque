3b97ee9dc7c2aa451419ab2b8d1462bc76e43587
Aluno <Aluno@10.1.20.34>
2015-11-16 22:59:58 -0200

    Feito na aula05

--- index.php
+++ index.php
@@ -78,8 +78,46 @@
 
         <div id="section1" class="container-fluid">
             <h1>Início</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <!-- carousel de imagens -->
+                            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                  <!-- Indicators -->
+                  <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                  </ol>
+
+                  <!-- Wrapper for slides -->
+                  <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                      <img src="http://susancorso.com/seedsforsanctuary/wp-content/uploads/2013/04/box_placeholder.jpg" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                      <img src="https://cloud.canadastays.com/assets/images/user-placeholder.png" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                      <img src="http://greenweddingshoes.com/wp-content/uploads/2011/09/DIY-terrarium-placeholder-03.jpg" alt="Flower">
+                    </div>
+
+                    <div class="item">
+                      <img src="http://greenweddingshoes.com/wp-content/uploads/2011/09/DIY-terrarium-placeholder-05.jpg" alt="Flower">
+                    </div>
+                  </div>
+
+                  <!-- Left and right controls -->
+                  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+            
         </div>
         <div id="section2" class="container-fluid">
             <h1>Cursos</h1>
--- js/meuscript.js
+++ js/meuscript.js
@@ -1,7 +1,19 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Add smooth scrolling to all links inside a navbar
+$("#myNavbar a").on('click', function(event){
 
+  // Prevent default anchor click behavior
+  event.preventDefault();
 
+  // Store hash (#)
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+});
